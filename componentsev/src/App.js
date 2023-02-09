5d164a2d2d24a1c4b08da1913aca2ff8f8c36126
muazam-cmd <muazamshahid7@gmail.com>
2023-02-09 12:01:13 +0400

    creating component and pages folder

--- componentsev/src/App.js
+++ componentsev/src/App.js
@@ -1,5 +1,5 @@
 import { GoBell, GoCloudDownload, GoDatabase } from 'react-icons/go';
-import Button from './Button';
+import Button from './components/Button';
 
 function App() {
   const handleClick = () => {
---
+++ componentsev/src/pages/ButtonPage.js
@@ -0,0 +1,49 @@
+import { GoBell, GoCloudDownload, GoDatabase } from 'react-icons/go';
+import Button from '../components/Button';
+
+function ButtonPage() {
+    const handleClick = () => {
+        console.log('Clicked!');
+    };
+
+    return (
+        <div>
+            <div>
+                <Button
+                    secondary
+                    outline
+                    rounded
+                    className="mb-5"
+                    onClick={handleClick}
+                >
+                    <GoBell />
+                    Click me!!
+        </Button>
+            </div>
+            <div>
+                <Button danger outline onMouseEnter={handleClick}>
+                    <GoCloudDownload />
+                    Buy Now!
+        </Button>
+            </div>
+            <div>
+                <Button warning onMouseLeave={handleClick}>
+                    <GoDatabase />
+                    See Deal!
+        </Button>
+            </div>
+            <div>
+                <Button secondary outline>
+                    Hide Ads!
+        </Button>
+            </div>
+            <div>
+                <Button primary rounded>
+                    Something!
+        </Button>
+            </div>
+        </div>
+    );
+}
+
+export default ButtonPage;
